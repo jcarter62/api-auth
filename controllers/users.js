8a0ff25a747eecef58d305adb7f8f56f98277a68
Jim Carter <jcarter62@gmail.com>
2018-08-17 13:29:53 -0700

    misc

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,20 @@
 module.exports = {
     signUp: async( req, res, next ) => {
+        //
+        // expect email and password
+        //
         console.log('UsersController.signup() called!');
         res.send({'function':'signUp'});
     },
     signIn: async( req, res, next ) => {
+        //
+        // generate token
+        //
         console.log('UsersController.signIn() called!');
         res.send({'function':'signIn'});
     },
     secret: async( req, res, next ) => {
+        //
         console.log('UsersController.secret() called!');
         res.send({'function':'secret'});
     }
